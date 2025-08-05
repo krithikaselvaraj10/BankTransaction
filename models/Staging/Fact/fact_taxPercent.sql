28d0aedce086ca7a19ac1d6e92e99273ffbe8225
krithika Selvaraj <krithikaselvaraj9918@gmail.com>
2025-08-05 05:55:09 +0000

    Commit demo

--- models/Staging/Fact/fact_taxPercent.sql
+++
@@ -1,4 +0,0 @@
-select distinct d.cc_call_center_id, d.cc_tax_percentage ,c.C_SALUTATION 
-from {{ source('Demo', 'customer') }} as c join {{ source('Demo', 'CALL_CENTER') }}  as d 
-on c.c_customer_id = d.cc_call_center_id 
-where d.cc_tax_percentage > 0
--- models/Staging/dateFetch.sql
+++
@@ -1,3 +0,0 @@
-select CC_CALL_CENTER_ID,cc_rec_start_date 
-from {{ source('Demo', 'CALL_CENTER') }}   
-where cc_rec_start_date > '1998-01-01'
